a49044a02d9ecfe365c15ec6492babb2f5befef2
Deepa S Chebbi <149763585+Deepa-S-Chebbi@users.noreply.github.com>
2025-09-30 00:59:06 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,7 +28,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="./C.V.pdf"
+                href="./src/assests/C.V.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
