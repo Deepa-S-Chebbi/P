64160d956b8b2fabde7ab528d9f70756739cf55c
Deepa S Chebbi <149763585+Deepa-S-Chebbi@users.noreply.github.com>
2025-10-03 19:51:23 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,4 +1,5 @@
 import { Briefcase, Code, User } from "lucide-react";
+import cvPdf from '../assets/C.V.pdf'; 
 
 export const AboutSection = () => {
   return (
@@ -28,7 +29,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="../assets/C.V.pdf"
+                href= { cvPdf }
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
